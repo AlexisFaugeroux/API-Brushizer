95b0d7558dae9807a6a47df60c0c1cbff255b296
AlexisFaugeroux <faugeroux.alexis@gmail.com>
2022-12-01 15:23:34 +0100

    Initial constraints added

--- migrations/revert/initial_constraints.sql
+++ migrations/revert/initial_constraints.sql
@@ -2,6 +2,22 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+ALTER TABLE "attribute"
+    DROP CONSTRAINT "consistant_date";
+ALTER TABLE "artwork"
+    DROP CONSTRAINT "consistant_date";
+ALTER TABLE "collection"
+    DROP CONSTRAINT "consistant_date";
+ALTER TABLE "user"
+    DROP CONSTRAINT "consistant_date";
+
+ALTER TABLE "user"
+    ALTER COLUMN "email" TYPE TEXT;
+DROP DOMAIN email;
+
+ALTER TABLE "artwork"
+    ALTER COLUMN "price_usd" TYPE NUMERIC,
+    ALTER COLUMN "price_sol" TYPE NUMERIC;
+DROP DOMAIN posnum;
 
 COMMIT;

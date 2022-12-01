f87de9600de06de82c647d1b695796ae305f9fe0
AlexisFaugeroux <faugeroux.alexis@gmail.com>
2022-12-01 11:33:30 +0100

    Deploy tables script done

--- migrations/deploy/structure.sql
+++ migrations/deploy/structure.sql
@@ -2,5 +2,60 @@
 
 BEGIN;
 
+CREATE TABLE "role"(
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user" TEXT NOT NULL,
+    "creator" TEXT NOT NULL,
+    "admin" TEXT NOT NULL
+);
+
+CREATE TABLE "user"(
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "email" TEXT NOT NULL UNIQUE,
+    "password" TEXT NOT NULL,
+    "pseudo" TEXT NOT NULL UNIQUE,
+    "profile_pic" TEXT,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ,
+    "role_id" INT NOT NULL REFERENCES "role"("id")
+);
+
+CREATE TABLE "collection"(
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "artist_name" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ,
+    "user_id" INT NOT NULL REFERENCES "user"("id")
+);
+
+CREATE TABLE "artwork"(
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL UNIQUE,
+    "description" TEXT NOT NULL,
+    "artist_name" TEXT NOT NULL,
+    "price_usd" NUMERIC NOT NULL,
+    "price_sol" NUMERIC NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ,
+    "user_id" INT NOT NULL REFERENCES "user"("id"),
+    "collection_id" INT NOT NULL REFERENCES "collection"("id")
+);
+
+CREATE TABLE "attribute"(
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "background" TEXT NOT NULL,
+    "shape" TEXT NOT NULL UNIQUE,
+    "shape_color" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "artwork_has_attribute"(
+    "artwork_id" INT NOT NULL REFERENCES "artwork"("id"),
+    "collection_id" INT NOT NULL REFERENCES "collection"("id"),
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now()
+);
 
 COMMIT;

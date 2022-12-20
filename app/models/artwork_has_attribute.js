c10f10272dcd0373098601ee2000e4307b088828
AlexisFaugeroux <faugeroux.alexis@gmail.com>
2022-12-20 14:41:53 +0100

    Fix delete one artwork

--- app/models/artwork_has_attribute.js
+++ app/models/artwork_has_attribute.js
@@ -4,11 +4,11 @@ export default class ArtworkHasAttribute extends CoreDatamapper {
     tableName = 'artwork_has_attribute';
 
     /**
-     * Delete artwork foreign key in table
+     * Delete several artworks foreign keys in table
      * @param {array} [id] list of record identifier
      * @returns {number} number of records deleted (truthy/falsy)
      */
-    async deleteArtworkFkeysRecords(identifiers) {
+    async deleteArtworksFkeysRecords(identifiers) {
         const placeHolders = [];
         let indexPlaceHolder = 1;
 
@@ -20,6 +20,22 @@ export default class ArtworkHasAttribute extends CoreDatamapper {
         const preparedQuery = {
             text: `DELETE FROM "${this.tableName}" WHERE artwork_id IN (${placeHolders})`,
             values: identifiers,
+        };
+
+        const result = await this.client.query(preparedQuery);
+
+        return !!result.rowCount;
+    }
+
+    /**
+     * Delete one artwork foreign keys in table
+     * @param {number} id record identifier
+     * @returns {number} number of records deleted (truthy/falsy)
+     */
+    async deleteOneArtworkFkeyRecords(id) {
+        const preparedQuery = {
+            text: `DELETE FROM "${this.tableName}" WHERE artwork_id = ${id}`,
+            values: id,
         };
 
         const result = await this.client.query(preparedQuery);
